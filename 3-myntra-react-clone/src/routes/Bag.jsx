19fc7f00ff775802084c55d3d04de0480ef10bc7
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-06-10 17:26:44 +0530

    BagSummary

--- 3-myntra-react-clone/src/routes/Bag.jsx
+++ 3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,20 +1,21 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+
+import BagSummary from "../components/BagSummary";
 
 const Bag=()=>{
-    return ( <>
+    return ( 
         
     <main>
       <div className="bag-page">
         <div className="bag-items-container">
         </div>
+        <BagSummary/>
         <div className="bag-summary">
         </div>
 
       </div>
     </main>
     
- </>
+ 
     );
 
 }
