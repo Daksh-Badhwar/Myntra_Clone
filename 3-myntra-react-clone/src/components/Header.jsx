27dd602acd1b6f6b2cfa40d838287e6c31dd5e85
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-06-12 12:01:55 +0530

    Bag Functionality

--- 3-myntra-react-clone/src/components/Header.jsx
+++ 3-myntra-react-clone/src/components/Header.jsx
@@ -2,8 +2,13 @@ import { IoPersonSharp } from "react-icons/io5";
 import { FaHeart } from "react-icons/fa";
 import { IoBag } from "react-icons/io5";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const Header = ()=>{
+
+const bag= useSelector((store)=>store.bag);
+
+
     return(<header>
         <div className="logo_container">
             <Link to="/">
@@ -39,7 +44,7 @@ const Header = ()=>{
              to="/bag">
                 <IoBag />
                 <span className="action_name">Bag</span>
-                <span className="bag-item-count">0</span>
+                <span className="bag-item-count">{bag.length}</span>
             </Link>
         </div>
     </header>)
